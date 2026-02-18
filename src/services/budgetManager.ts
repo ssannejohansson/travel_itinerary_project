ea2cef807e8b3e0f462dd49e3f1bac36a2d7552a
ssannejohansson <sannej0hansson@hotmail.com>
2026-02-18 15:47:07 +0100

    Create high cost filter function

--- src/services/budgetManager.ts
+++ src/services/budgetManager.ts
@@ -1,12 +1,16 @@
-import type { Trip } from "../models.js";
+import type { Trip, Activity } from "../models.js";
 
+// Calculates the total cost of a trip by summing the cost of all activities
 export const calculateTotalCost = (trip: Trip): number => {
     return trip.activities.reduce((sum, activity) => sum + activity.cost, 0);
 }
 
+// Returns all activities with a cost greater than or equal to the given threshold
+export const getHighCostActivities = (activities: Activity[], threshold: number) => {
+    return activities.filter(activity => activity.cost >= threshold);
+}
 
-// test 
-
+// Test, remove when real array is created
 const trip = {
     activities: [
         {
@@ -24,4 +28,12 @@ const trip = {
     ],
 };
 
-console.log(`Total: ${calculateTotalCost(trip)}`);
+//console.log(`Total: ${calculateTotalCost(trip)}`);
+console.log(
+  "High cost >= 500 should be Flight only:",
+  getHighCostActivities(trip.activities, 500),
+);
+console.log(
+  "High cost >= 100 should show all activities:",
+  getHighCostActivities(trip.activities, 100),
+);
