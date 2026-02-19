56fc5457bbbfeb02bcdb27b12d4f432eea605d7f
ssannejohansson <sannej0hansson@hotmail.com>
2026-02-19 17:57:31 +0100

    Add basecost to calculation

--- src/services/budgetManager.ts
+++ src/services/budgetManager.ts
@@ -1,8 +1,11 @@
 import type { Trip, Activity } from "../types/types.js";
+import { Cost } from "../types/types.js";
 
-// Calculates the total cost of a trip by summing the cost of all activities
+// Calculates the total cost of a trip by summing the cost the trip + cost of all activities
 export const calculateTotalCost = (trip: Trip): number => {
-	return trip.activities.reduce((sum, activity) => sum + activity.cost, 0);
+    const baseCost = Cost[trip.destination as keyof typeof Cost];
+	const activitiesTotal = trip.activities.reduce((sum, activity) => sum + activity.activityCost, 0);
+    return baseCost + activitiesTotal;
 };
 
 // Returns all activities with a cost greater than or equal to the given threshold
@@ -10,7 +13,7 @@ export const getHighCostActivities = (
 	activities: Activity[],
 	threshold: number,
 ) => {
-	return activities.filter((activity) => activity.cost >= threshold);
+	return activities.filter((activity) => activity.activityCost >= threshold);
 };
 
 // Test, remove when real array is created
