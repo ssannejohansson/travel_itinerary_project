195f5d74383fbfbdb51272bc1e0520f2da13258e
eklownr <eklownr@gmail.com>
2026-02-19 12:12:25 +0100

    update acitityCLI, maxBudgetCLI, viewBudgetCLI

--- src/cliHandlers/activityCLI.ts
+++ src/cliHandlers/activityCLI.ts
@@ -1,78 +1,8 @@
-import inquirer from "inquirer";
 import { mainMenu } from "../mainCli.js";
-import { color } from "../services/formatUtils.js";
-import { type Trip } from "../types/types.js";
-import { ActivityCost } from "../types/types.js";
-import { type ActivityType } from "../types/types.js";
 
-/**
- * Handle activity options for user
- * @param user - from mainMenu - instead of global user
- * @param counter - how many activities do user have
- */
-export const activityMenu = async (
-	user: Trip,
-	counter: number,
-): Promise<void> => {
-	try {
-		// Handle users options
-		const activity = await inquirer.prompt<{ selectActivity: string }>([
-			{
-				type: "select",
-				name: "selectActivity",
-				message: `Select an activity:
- ******************************
- * Food		- ${ActivityCost.food} kr, 
- * Transport 	- ${ActivityCost.transport} kr, 
- * Sightseeing 	- ${ActivityCost.sightseeing} kr
- * No activity 	- 0 kr
- * ******************************`,
+export const activityMenu = (user: Trip, activityCounter: number) => {
+	console.log("activityMenu");
 
-				choices: [`food`, `transport`, `sightseeing`, `no activity`],
-			},
-		]);
-
-		/**
-		 * Add data to user object
-		 */
-
-		// Check if ActivityCost has valid type, returns true or false
-		const isValidActivity = (value: string): value is ActivityType => {
-			return value in ActivityCost;
-		};
-
-		// Store activity to user object
-		if (
-			// check if activity is not "no activity" or undefined
-			activity.selectActivity !== "no activity" &&
-			user.activities[counter] !== undefined &&
-			typeof activity.selectActivity === "string" &&
-			isValidActivity(activity.selectActivity)
-		) {
-			// add activity, startTime and activityCost
-			user.activities[counter].name = activity.selectActivity;
-			user.activities[counter].startTime = user.startDate;
-			user.activities[counter].activityCost =
-				ActivityCost[activity.selectActivity];
-		}
-
-		// Print out selected activity in green
-		console.log(
-			color("green", `Your activity is ${activity.selectActivity}`),
-		);
-
-		// User data so far TEST
-		// console.log(color("red", "User data so far: "), user);
-
-		// Back to main-menu again
-		mainMenu();
-
-		// Handle errors
-	} catch (error) {
-		if (error instanceof Error) {
-			console.error("Menu error:", error.message);
-		} else {
-			console.error("Unknown error:", String(error));
-		}
-	}
+	// go back to main menu
+	mainMenu();
 };
--- src/cliHandlers/viewBudgetCLI.ts
+++ src/cliHandlers/viewBudgetCLI.ts
@@ -1,36 +1,8 @@
 import { mainMenu } from "../mainCli.js";
-import { type Trip } from "../types/types.js";
-import { color } from "../services/formatUtils.js";
 
-export const viewBudgetMenu = async (user: Trip): Promise<number> => {
-	try {
-		console.log(color("blue", "*".repeat(30)));
+export const viewBudgetMenu = (user: Trip) => {
+	console.log("viewBudgetMenu");
 
-		console.log(`View budget, ${color("yellow", "Trip + activities:")}`);
-		console.log(`Trip to ${user.destination} cost: ${user.cost} kr`);
-		const totalCost =
-			user.cost +
-			user.activities.reduce(
-				(total, activity) => total + activity.activityCost,
-				0,
-			);
-		user.activities.forEach((activity) => {
-			if (activity.name !== "No activity set") {
-				console.log(`${activity.name} - ${activity.activityCost} kr`);
-			}
-		});
-		console.log(color("cyan", `Total cost: ${totalCost} kr`));
-
-		console.log(color("blue", "*".repeat(30)));
-
-		mainMenu();
-		return totalCost;
-	} catch (error) {
-		if (error instanceof Error) {
-			console.error("Menu error:", error.message);
-		} else {
-			console.error("Unknown error:", String(error));
-		}
-		throw error;
-	}
+	// Go back to main menu
+	mainMenu();
 };
