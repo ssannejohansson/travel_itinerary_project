f0693882676fc6dc41bdb35e5ff85e9adaf1c840
ssannejohansson <sannej0hansson@hotmail.com>
2026-02-19 17:51:39 +0100

    Add budgetMenuCLI

--- src/services/budgetManager.ts
+++ src/services/budgetManager.ts
@@ -2,7 +2,7 @@ import type { Trip, Activity } from "../types/types.js";
 
 // Calculates the total cost of a trip by summing the cost of all activities
 export const calculateTotalCost = (trip: Trip): number => {
-	return trip.activities.reduce((sum, activity) => sum + activity.cost, 0);
+	return trip.activities.reduce((sum, activity) => sum + activity.activityCost, 0);
 };
 
 // Returns all activities with a cost greater than or equal to the given threshold
@@ -10,7 +10,7 @@ export const getHighCostActivities = (
 	activities: Activity[],
 	threshold: number,
 ) => {
-	return activities.filter((activity) => activity.cost >= threshold);
+	return activities.filter((activity) => activity.activityCost >= threshold);
 };
 
 // Test, remove when real array is created
