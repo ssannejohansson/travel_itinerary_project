02fac474742dd08172445cfa97dc419ed0fc9789
maria-techhub <mariakiran.tech@gmail.com>
2026-02-20 10:21:33 +0100

    add activities

---
+++ src/services/itineraryEngine.ts
@@ -0,0 +1,7 @@
+import type { Trip, Activity } from "../models.js";
+
+//addActivitiesToTrip();with details like name, cost, category, and time
+
+//viewAllActivitiesForDay();
+//view all activities for a specific day to see my daily schedule.
+//filterActivitiesByCategory();
