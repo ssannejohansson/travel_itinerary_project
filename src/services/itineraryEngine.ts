b29d7fd4f2c706afd6b3d4c7e9ef6eaea05e5083
maria-techhub <mariakiran.tech@gmail.com>
2026-02-20 10:28:59 +0100

    add activities

--- src/services/itineraryEngine.ts
+++ src/services/itineraryEngine.ts
@@ -1,6 +1,32 @@
-import type { Trip, Activity } from "../models.js";
+import type { Trip, Activity } from "../types/types.js";
+
+import Readline from "readline";
 
 //addActivitiesToTrip();with details like name, cost, category, and time
+
+// Create readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// Add a new activity
+const addActivity = (): void => {
+    rl.question("Enter task: ", (text: string) => {
+        if (text.trim() === "") {
+            console.log("Task cannot be empty!\n");
+        } else {
+            const newTodo: Todo = {
+                id: Date.now(),
+                text: text.trim(),
+            };
+
+            todos.push(newTodo);
+            console.log("? Task added successfully!\n");
+        }
+        showMenu();
+    });
+};
 
 //viewAllActivitiesForDay();
 //view all activities for a specific day to see my daily schedule.
